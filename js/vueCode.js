da7fc971b84b52ebb26d2bbc4e2515a6f4ffad80
Sandro81 <sandro.fioravanti81@gmail.com>
2020-02-13 20:09:43 +0000

    animation.css

--- js/vueCode.js
+++ js/vueCode.js
@@ -6,6 +6,19 @@ new Vue({
         imgCard: [
             {name:"yellow", url: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOgAAADZCAMAAAAdUYxCAAAAA1BMVEX/609CC38RAAAAR0lEQVR4nO3BAQEAAACCIP+vbkhAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAALcGxYEAAVIv8uIAAAAASUVORK5CYII="},
             {name:"red", url:"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAA1BMVEX/AAAZ4gk3AAAASElEQVR4nO3BgQAAAADDoPlTX+AIVQEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADwDcaiAAFXD1ujAAAAAElFTkSuQmCC"},
-        ]
+        ],
+        show: true
+    },
+    methods: {
+        showMethod: function (event) {
+            this.show = !this.show;
+            // `this` inside methods points to the Vue instance
+            //alert('Hello ' + this.name + '!')
+            // `event` is the native DOM event
+            if (event) {
+
+                //alert(event.target.tagName)
+            }
+        }
     }
 });
